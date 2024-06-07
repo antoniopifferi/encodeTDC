72c82367e72b91add2e8407cf4a0b931f14c74b4
Antonio Pifferi <antonio.pifferi@polimi.it>
2024-06-07 12:26:31 +0200

    Python File

---
+++ encodeTDC.py
@@ -0,0 +1,28 @@
+from snAPI.Main import *
+
+if(__name__ == "__main__"):
+
+    # PTU data
+    start = 0
+    length = 10
+    
+    sn = snAPI()
+    sn.getFileDevice(r"HIGH_0000.ptu")
+    sn.getDeviceConfig()
+    #sn.logPrint(json.dumps(sn.deviceConfig, indent=2))
+    
+    sn.getMeasDescription()
+    #sn.logPrint(json.dumps(sn.measDescription, indent=2))
+    
+    sn.unfold.measure(acqTime=1000, size=int(sn.measDescription["NumRecs"]), waitFinished=True)
+    times, channels  = sn.unfold.getData()
+    #sn.logPrint(f"Unfold data records: {len(times)}")
+    #sn.logPrint("  channel |  absTime") 
+    #sn.logPrint("--------------------")
+    
+    for i in range(start,start+length):
+        a=1
+        #sn.logPrint(f"{channels[i]:9} | {times[i]:8}")
+ 
+    #plot(sn.histogram())
+    
